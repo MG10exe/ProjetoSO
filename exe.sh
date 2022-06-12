e2d39266b2e51210b53354a2093a65d3731664d3
Matheus Gomes <61914401+MG10exe@users.noreply.github.com>
2022-06-12 19:55:25 -0300

    Update exe.sh

--- exe.sh
+++ exe.sh
@@ -3,4 +3,4 @@
 echo "Digite o nome do arquivo a ser executado: "
 read file
 
-for ((n=$1; n<=$2; n++)); do ./$file; done
+for ((n=$1; n<=$2; n++)); do /usr/bin/time --format="%E %U %S %M" ./u.sh 2>> teste.txt ./$file; done
