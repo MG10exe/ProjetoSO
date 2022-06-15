ebfe381fc54b11acea5927f0cec7f864722fa497
Matheus Gomes <61914401+MG10exe@users.noreply.github.com>
2022-06-15 10:59:04 -0300

    Update gerandoGraficos.sh

--- gerandoGraficos.sh
+++ gerandoGraficos.sh
@@ -6,12 +6,12 @@ KERNEL
 MEM" > a
 
 #Gerando arquivo com dados dos tempos de execução
-paste a mu2.sh.txt mu.sh.txt | sed '$d' > hhhh
+paste a msemThreads.py.txt musandoThreads.py.txt | sed '$d' > dados
 
 #Gerando gráfico dos tempos de execução
 gnuplot < grafico.plt
 
 #Gerando arquivo com dados do uso de memória
-paste a mu2.sh.txt mu.sh.txt | sed '1,3d' > hhhh
+paste a msemThreads.py.txt musandoThreads.py.txt | sed '1,3d' > dados
 
 gnuplot < grafico2.plt
