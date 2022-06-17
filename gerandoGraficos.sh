7d4a441190acc08d91a4afb806d6e534e4f82b5b
Matheus Gomes <61914401+MG10exe@users.noreply.github.com>
2022-06-17 10:42:33 -0300

    Update gerandoGraficos.sh

--- gerandoGraficos.sh
+++ gerandoGraficos.sh
@@ -14,4 +14,9 @@ gnuplot < graficosTempos.plt
 #Gerando arquivo com dados do uso de memória
 paste a msemThreads.py.txt musandoThreads.py.txt | sed '1,3d' > dados
 
+#Gerando gráfico de uso de memória
 gnuplot < graficosMemoria.plt
+
+#removendo arquivos
+rm dadossemThreads.py.txt dadosusandoThreads.py.txt semThreads.py.txt usandoThreads.py.txt
+rm msemThreads.py.txt musandoThreads.py.txt a dados
